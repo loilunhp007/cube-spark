b39c4faf777bdb9900a06ed825bc90beb42ee59e
TikTzuki <tranphanthanhlong18@gmail.com>
2020-08-06 05:06:50 +0700

    6/8

---
+++ demo_cube/src/test/java/cube_spark/example/demo_cube/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cube_spark.example.demo_cube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
