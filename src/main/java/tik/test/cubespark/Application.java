29fa3b1bf33fab5a7d969ca091c0d50c2705e6e6
TikTzuki <tranphanthanhlong18@gmail.com>
2020-10-03 07:27:53 +0700

    3/10

--- src/main/java/tik/test/cubespark/Application.java
+++ src/main/java/tik/test/cubespark/Application.java
@@ -8,5 +8,6 @@ import org.springframework.context.ApplicationContext;
 public class Application {
 		public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(Application.class, args);
+		
 	}
 }
