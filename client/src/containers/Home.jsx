b289ca8262839d29319c91799ed0f32f211a9ce6
Tiktzuki <tranphanthanlong18@gmail.com>
2020-10-11 21:22:01 +0700

    11/10

--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -1,13 +1,25 @@
+import { Grid } from "@material-ui/core";
 import React from "react";
 import {Link} from "react-router-dom";
 import FuseNavbar from "../components/fuse-navbar";
-
-export default function Home() {
-  return (
+import Header from "../components/header";
+/* 
     <div className="flex h-screen">
         <h3>Welcome Home</h3>
         <Link to='/authorization'>Authorization</Link>
-        <FuseNavbar/>
-    </div>
+*/
+export default function Home() {
+  return (
+    <Grid container direction="row">
+      <Grid item xs={2}>
+        Nav
+      </Grid>
+      <Grid item xs={10}>
+        <Grid container direction="column">
+            <Header/>
+        </Grid>
+      </Grid>
+    </Grid>
+
   );
 }
